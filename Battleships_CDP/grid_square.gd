fe266896cce84550b0b09dd3a6cdd4ecd540f143
xxaleksandraxx <aleksandra.te@int.pl>
2026-04-21 13:38:36 +0200

    Add files via upload
    Uploading godot assets for the UI wih UI logic for starting the game and creating player's board

---
+++ Battleships_CDP/grid_square.gd
@@ -0,0 +1,16 @@
+extends Button
+
+var coordinate: Vector2 = Vector2.ZERO
+var ship_id: int = -1 # -1 means no ship is here
+
+func set_coordinate(c: Vector2):
+	coordinate = c
+
+func mark_as_ship(id: int):
+	ship_id = id
+	# changing the color if the ship is places
+	self_modulate = Color("ff88ff")
+
+func mark_as_water():
+	ship_id = -1
+	modulate = Color("89cff0") # resetting if necessaryy
