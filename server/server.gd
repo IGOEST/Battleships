6efc6f8ee9232c28f3bef7b224cbb84d79ff0292
Igor Estrop <s197686@student.pg.edu.pl>
2026-04-27 16:16:22 +0200

    Fixing game thread

--- server/server.gd
+++ server/server.gd
@@ -21,6 +21,8 @@ func _ready() -> void:
 		push_error("Server failed to start on port %d" % server_port)
 		return
 	print("Server started on port %d" % server_port)
+	game_thread = Thread.new()
+	game_thread.start(Callable(self, "_game_logic_thread"))
 
 func _process(delta: float) -> void:
 	if server.is_connection_available():
@@ -33,40 +35,11 @@ func _process(delta: float) -> void:
 		threads.append(t)
 		t.start(Callable(self, "_client_thread").bind(client))
 		
-		game_thread = Thread.new()
-		game_thread.start(Callable(self, "_game_logic_thread"))
-		
 	mutex.lock()
 	while not outgoing_queue.is_empty():
 		var packet = outgoing_queue.pop_front()
 		send_to_client(packet["client"], packet["message"])
 	mutex.unlock()
-		
-	#for i in range(clients.size()):
-		#var client: StreamPeerTCP = clients[i]
-		#client.poll()
-			#
-		#if client.get_status() != StreamPeerTCP.STATUS_CONNECTED:
-			#print("Client disconnected!")
-			#client_buffers.erase(client)
-			#clients.remove_at(i)
-			#continue
-			
-		#if client.get_available_bytes() > 0:
-			#var bytes_available = client.get_available_bytes()
-			#var data = client.get_utf8_string(bytes_available)
-			#print("Raw data received(", bytes_available, " bytes): '", data, "'")
-			#client_buffers[client] += data
-		#
-		#while "\n" in client_buffers[client]:
-			#var newLine_pos = client_buffers[client].find("\n")
-			#var message = client_buffers[client].substr(0, newLine_pos)
-			#client_buffers[client] = client_buffers[client].substr(newLine_pos + 1)
-			#
-			#print("Message: ", message, " (length: ", message.length(), ")")
-			#if message.length() > 0:
-				#print("Processing message ", message)
-				#handle_client_message(client, message)
 				
 func _client_thread(client):
 	while client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
@@ -91,20 +64,6 @@ func _client_thread(client):
 	clients.erase(client)
 
 func handle_client_message(client: StreamPeerTCP, message: String):
-	#var parts = message.strip_edges().split(" ", false, 1)
-	#if parts.size() == 0:
-		#return
-	#
-	#var command = parts[0]
-	#var data = parts[1] if parts.size() > 1 else ""
-	#
-	#match command:
-		#"CALL_FUNCTION":
-			#call_server_function(client, data)
-		#"REQUEST_INFO":
-			#send_info_to_client(client)
-		#_:
-			#send_to_client(client, "ERROR: Unknown command")
 	print("SERVER: CLIENT THREAD - Received from client: ", message)
 	
 	mutex.lock()
@@ -167,4 +126,5 @@ func _exit_tree() -> void:
 		client.disconnect_from_host()
 	for t in threads:
 		t.wait_to_finish()
+	game_thread.wait_to_finish()
 	server.stop()
